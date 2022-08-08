b9d6a07114be97faa1945d9c25a85d8e56a4fd31
ThanhHa <june25th87@gmail.com>
2022-08-08 22:35:24 +0700

    Add object mapper util class

---
+++ IceCream/Utils/ObjectMapper.cs
@@ -0,0 +1,43 @@
+﻿using IceCream.Models.DTO_Model.RequestDTO;
+using IceCream.Models.DTO_Model.ResponseDTO;
+using IceCream.Models.Entities;
+
+namespace IceCream.Utils;
+
+public static class ObjectMapper
+{
+    public static UserDisplayDTO? Convert(this User? u)
+    {
+        return u == null
+            ? null
+            : new UserDisplayDTO
+            {
+                UserId = u.UserId,
+                Name = u.Name,
+                Address = u.Address,
+                Contact = u.Contact,
+                Email = u.Email,
+                JoinDate = u.JoinDate,
+                CardNo = u.CardNo,
+                IsActive = u.IsActive,
+                UserType = u.UserType
+            };
+    }
+
+    public static User? Convert(this UserRegisterDTO? u)
+    {
+        return u == null
+            ? null
+            : new User
+            {
+                Name = u.Name,
+                Address = u.Address,
+                Contact = u.Contact,
+                Email = u.Email,
+                JoinDate = u.JoinDate,
+                CardNo = u.CardNo,
+                IsActive = u.IsActive,
+                UserType = u.UserType
+            };
+    }
+}
